03f5e38aed6704e8507811cbcc5a340bad033dad
Ahammed nihal pt <ahammednihalpt@gmail.com>
2023-03-31 18:21:25 +0530

    ssl update

--- frontend/src/Components/LandingPageComponents/LandingPageContent/About/About.js
+++ frontend/src/Components/LandingPageComponents/LandingPageContent/About/About.js
@@ -8,7 +8,7 @@ function About() {
         <h1 className="focus-in-expand-fwd mt-5">About</h1>
         <div className="about_main">
           <div className=" about_image">
-            <img src="http://142.93.165.76:9000/image/aboutimg.jpg" alt="" />
+            <img src="https://142.93.165.76:9000/image/aboutimg.jpg" alt="" />
           </div>
           <div className="about_text">
             <h4>A Vision of Better Building.</h4>
--- frontend/src/Helpers/config/axiosInstance.js
+++ frontend/src/Helpers/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://142.93.165.76:9000/",
+  baseURL: "https://142.93.165.76/:9000/",
   // timeout: 1000,
   headers: { "X-Custom-Header": "foobar" },
 });
