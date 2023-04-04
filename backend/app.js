bba97caf765b98eaa014bc6cf381a4cd42dca779
Ahammed nihal pt <ahammednihalpt@gmail.com>
2023-04-04 13:43:08 +0530

    cors change

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ const app = express();
 
 app.use(cors());
 dotenv.config();
+app.options('*', cors());
 app.use(fileupload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
